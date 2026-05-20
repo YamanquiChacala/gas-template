b381f8d28536ea2947259235eac557408656ea88
Yamanqui García Rosales <yamanqui@gmail.com>
2026-05-19 18:20:54 -0700

    Added test coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,5 +11,17 @@ module.exports = {
     testPathIgnorePatterns: [
         "/node_modules/",
         "\\.gas\\.test\\.ts$" // Tells Jest to completely ignore these cloud tests
-    ]
+    ],
+
+    collectCoverageFrom: [
+        "src/common/**/*.ts", // ONLY track coverage for files in src/common
+        "!src/common/**/*.test.ts", // Exclude standard Jest test files from the report
+        "!src/common/**/*.gas.test.ts", // Exclude GAS test files from the report
+        "!src/common/**/*.d.ts", // Exclude TypeScript definition files
+        "!src/common/constants/*",
+        "!src/common/gas-parts/*",
+    ],
+    // 'text' gives the terminal output. 'html' gives the browser report.
+    // 'lcov' creates the lcov.info file needed for VSCode Coverage Gutter.
+    coverageReporters: ["text", "html", "lcov"],
 };
